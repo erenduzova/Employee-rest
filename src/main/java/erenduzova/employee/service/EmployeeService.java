966c28405ad31b60857270556ee303c8e0a89597
Eren Düzova <erenduzova@gmail.com>
2023-11-02 21:58:48 +0300

    Service package created. EmployeeService interface created. EmployeeServiceImpl implementation created.   findAll() method added.

---
+++ src/main/java/erenduzova/employee/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package erenduzova.employee.service;
+
+import erenduzova.employee.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+}
---
+++ src/main/java/erenduzova/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+package erenduzova.employee.service;
+
+import erenduzova.employee.dao.EmployeeDAO;
+import erenduzova.employee.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
